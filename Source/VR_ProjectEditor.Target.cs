fab1ca320f47466d4df06b781c4db7113c68796b
ryanlui16055 <ryanlui1605@gmail.com>
2022-01-16 23:04:41 +0800

    Initial commit

---
+++ Source/VR_ProjectEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class VR_ProjectEditorTarget : TargetRules
+{
+	public VR_ProjectEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "VR_Project" } );
+	}
+}
